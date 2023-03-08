99221a7ee41868fd875f9614909fbd7bb7141b98
ahambrahmashmi <11799abhishek@gmail.com>
2023-03-08 15:14:09 +0530

    logs

--- src/modules/productDetails/index.tsx
+++ src/modules/productDetails/index.tsx
@@ -1,22 +1,62 @@
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Dimensions, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
-
+import { normalize } from '../../utils/dimensions';
+const {width} = Dimensions.get('screen');
 const ProductDetails = ({route}: any) => {
   const {title, description, image, price} = route.params;
   console.log('====', title, description);
 
   return (
-    <View>
-      <Text>{title}</Text>
+    <SafeAreaView>
+      {/* <Text>{title}</Text>
       <Image
         source={{uri: image}}
         style={{resizeMode: 'contain', height: 50, width: 50}}
       />
-      <Text>{price}</Text>
-    </View>
+      <Text>{price}</Text> */}
+
+
+
+<Image source={{uri:image}} style={styles.renderImg} />
+        <TouchableOpacity>
+          <Text numberOfLines={1} style={styles.titleStyle}>
+            {title}
+          </Text>
+          <Text style={styles.descStyle} numberOfLines={1}>
+            {description}
+          </Text>
+          <View style={styles.priceView}>
+            <Text style={styles.priceColor}>${price}</Text>
+          </View>
+        </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
 export default ProductDetails;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  renderImg: {
+    height: normalize(90),
+    width: normalize(90),
+    resizeMode: 'contain',
+  },
+  titleStyle: {
+    width: (width * 2) / 3,
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  descStyle: {
+    width: (width * 2) / 3,
+    textAlign: 'justify',
+  },
+  priceView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: normalize(10),
+  },
+  priceColor: {
+    color: 'green',
+    fontSize: 18,
+  },
+});
